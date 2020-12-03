f73e69e61b9eaaccc445112b534f7cac6d3db0a5
Corey Mayo <coremayo@gmail.com>
2020-12-03 10:37:32 -0500

    version 0.4.22

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name = 'Geraldo',
-    version = '0.4.21',
+    version = '0.4.22',
     description = 'Geraldo is a reports engine for Python and Django applications',
     long_description = 'Geraldo is a Python and Django pluggable application that works with ReportLab to generate complex reports.',
     author = 'Marinho Brandao',
